2aa054861a3c27519fb9edce2c1c519d1cd35f92
AugustoV2 <augustovarghese@gmail.com>
2024-08-01 00:49:29 +0530

    test3

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,7 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 import {Details} from "../../../prisma/index";
+import read from "../../../prisma/index";
+import update from "../../../prisma/index"
 
 export async function POST(req: NextRequest) {
-  return await Details(req);
+ update();
+ return NextResponse.json({ message: 'Hello - POST' });
 }
--- prisma/index.ts
+++ prisma/index.ts
@@ -3,36 +3,28 @@ import { NextRequest, NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
 
-// async function read() {
-//     const allUsers = await prisma.user.findMany()
-//         console.log(allUsers)
+ export default async function read() {
+     const allUsers = await prisma.info.findMany()
+        console.log(allUsers)
 
-// }
+}
 
-//  async function Write() {
-//     const pr = await prisma.user.create({
-//         data: {
-//             name: 'Alice',
-//             email: 'hello@gmail.com'
-//         },
-//     })
 
-//     console.log(pr)
-// }
+ export async function  update(){
+    const pr = await prisma.info.update({
+        where : {id: '66aa83c7ebe928e4641d5618', firstname: 'sss'},
+        data: {firstname: 'bob'},
+    })
+    console.log(pr);
+    
 
-// async function update() {
-//     const user = await prisma.user.update({
-//         where: { id: '1' },
-//         data: { name: 'Bob' },
-//     })
-//     console.log(user)
-// }
+ }
 
 
 export async function Details(request: NextRequest) {
     const detailsform = await request.json();
     const datafield = detailsform.newdata;
-        const pr = await prisma.info.create({
+        const pr =  prisma.info.create({
             data:{...datafield}
         })
     
