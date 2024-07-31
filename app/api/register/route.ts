462abdebe97ea17b1ecda40443820072ad841bf3
AugustoV2 <augustovarghese@gmail.com>
2024-08-01 01:44:12 +0530

    test4

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 
-import {Details} from "../../../prisma/index";
+import {deleteRecord, Details} from "../../../prisma/index";
 import read from "../../../prisma/index";
 import update from "../../../prisma/index"
 
+
 export async function POST(req: NextRequest) {
- update();
+ deleteRecord();
  return NextResponse.json({ message: 'Hello - POST' });
 }
--- prisma/index.ts
+++ prisma/index.ts
@@ -3,33 +3,53 @@ import { NextRequest, NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
 
- export default async function read() {
-     const allUsers = await prisma.info.findMany()
-        console.log(allUsers)
+export default async function read() {
+    const allUsers = await prisma.info.findMany()
+    console.log(allUsers)
 
 }
 
 
- export async function  update(){
-    const pr = await prisma.info.update({
-        where : {id: '66aa83c7ebe928e4641d5618', firstname: 'sss'},
-        data: {firstname: 'bob'},
-    })
-    console.log(pr);
+export async function update() {
+    try {
+        const pr = await prisma.info.update({
+            where: { id: '66aa83c7ebe928e4641d5618' },
+            data: { firstname: 'bob' },
+        })
+        console.log(pr);
+
+    } catch (error) {
+        console.error('Error updating record:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+
+export async function deleteRecord(){
+    const pr= prisma.info.delete({
+        where: {id :'66aa83c7ebe928e4641d5618'},
+        
+        
+    });
+    return pr;
+}
+       
+
+
     
 
- }
 
 
 export async function Details(request: NextRequest) {
     const detailsform = await request.json();
     const datafield = detailsform.newdata;
-        const pr =  prisma.info.create({
-            data:{...datafield}
-        })
-    
-        console.log(pr)
-    
+    const pr = prisma.info.create({
+        data: { ...datafield }
+    })
+
+    console.log(pr)
+
     return NextResponse.json({ message: 'Hello - POST' });
 }
 
